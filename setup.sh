6eae970000b16826e935a33ac2edc719ad82b0c1
Andre Vauvelle <vauvelleandre@gmail.com>
2022-10-25 16:18:49 +0100

    fix typo

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 # Install starship
 conda install -c conda-forge starship
 # Install zplug
-$ curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 
 ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/zshrc/.zshrc ~/.zshrc
